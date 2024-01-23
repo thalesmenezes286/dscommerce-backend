7f8f703b1d60fc0fa21cecb436dc9a71d58ea4a0
Thales Ricardo Menezes <thalesmenezes286@gmail.com>
2024-01-23 18:55:50 -0300

    feat: validação de erros, controller, dto, service de product

--- dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,16 +1,23 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
-
-    public ProductDTO(){}
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/exceptions/DatabaseEXception.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscommerce.services.exceptions;
+
+public class DatabaseEXception extends RuntimeException{
+
+    public DatabaseEXception(String msg){
+        super(msg);
+    }
+}
